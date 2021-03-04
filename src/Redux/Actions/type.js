0932c8ced8d83dd85ac24942a86c93923dfc6117
payal <payal.agrawal@sparxitsolutions.com>
2021-03-04 13:46:48 +0530

    Updates

--- src/Redux/Actions/type.js
+++
@@ -1,8 +0,0 @@
-export const LoadTableSuccess = "LoadTableSuccess";
-export const LoadTableFail = "LoadTableFail";
-export const LoadFormSuccess = "LoadFormSuccess";
-export const LoadFormFail = "LoadFormFail";
-export const LoadUpdateSuccess = "LoadUpdateSuccess";
-export const LoadUpadteFail = "LoadUpadteFail";
-export const CreateDataSucess = "CreateDataSucess";
-export const CreateDataFail = "CreateDataFail";
